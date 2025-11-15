5c5cb8a8d8c215e63b6cb58c6b50bb69b12e24b7
sb1002 <sneha42b@gmail.com>
2025-11-15 14:10:15 -0500

    move CardGrid to testing file

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,10 +1,19 @@
 import Card from "@/components/card";
-import CardGrid from "@/components/CardGrid";
 
 export default function testing() {
-    return (
-        <div className="bg-white h-screen flex justify-center">
-            <CardGrid />
+    const items = [
+        { org: "Org A", desc: "Description A" },
+        { org: "Org B", desc: "Description B" },
+        { org: "Org C", desc: "Description C" },
+        { org: "Org D", desc: "Description D" },
+        // add as many as you want
+      ];
+    
+      return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+          {items.map((item, i) => (
+            <Card key={i} org={item.org} desc={item.desc} />
+          ))}
         </div>
-    )
+      );
 }
